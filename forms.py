69206927e9ebaeae0a413c4f289369f03aaabc18
clexp <cm.lewis@pm.me>
2023-02-10 16:17:00 +0000

    update forms

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms.fields.html5 import StringField, SubmitField, PasswordField, EmailField
+from wtforms.fields import StringField, SubmitField, PasswordField, EmailField
 from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditorField
 
